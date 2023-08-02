e0ed35c54148726403c8015ba7cdbc7dca0d9f17
Damyan Dimitrov <damqn_96@abv.bg>
2023-08-02 03:48:40 +0300

    Add initial infrastructure

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/damyan.dimitrov/gogardenofapples/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:3000
+}
